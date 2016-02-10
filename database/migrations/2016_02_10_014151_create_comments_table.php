c8d56cc86d91c5a9101c0d8e0a2e93a737cd8c04
brianShreckengast <bshreckengast@sparefoot.com>
2016-02-09 19:56:38 -0600

    added posts, comments, sub_user tables

---
+++ database/migrations/2016_02_10_014151_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('comment_id')->nullable()->unsigned();
+            $table->integer('post_id')->nullable()->unsigned();
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
